4e318e8932ed53db347d91f0a05d04c3cd6d694b
bahayg <bahayg@gmail.com>
2019-11-05 10:18:37 -0800

    Finish two methods

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,37 @@
 class Customer
   attr_accessor :given_name, :family_name
 
+  @@all = []
+
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @restaurants = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{given_name} #{family_name}"
   end
-end
+
+  def add_review(restaurant, rating)
+    Review.new(self, restaurant)
+  end
+
+  def num_reviews
+    Review.all.select {|reviews| reviews.customer == self}.length
+  end
+
+  # def self.find_by_name(name)
+  #   Review.all.find {|reviews| reviews.customer.full_name == name}.first
+  # end
+
+  # def self.find_all_by_given_name(name)
+
+  # end 
+
+end 
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+bahay = Customer.new("Gulle Bilgi", "Bahay")
+pinar = Customer.new("Bilgi", "Pinar")
+sevgi = Customer.new("Gulle", "Sevgi")
+
+mc = Restaurant.new("Mis Cafe")
+gc = Restaurant.new("Garlic Crush")
+rl = Restaurant.new("Red Lobster")
+
+r1 = Review.new(bahay, mc, 9)
+r2 = Review.new(pinar, gc, 7)
+r3 = Review.new(sevgi, rl, 8)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line

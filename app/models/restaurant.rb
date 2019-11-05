be07dd73690dceee8e6f746e5e5575848feffb9a
github username <github email address>
2019-11-05 10:22:54 -0800

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,21 @@
 class Restaurant
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self 
   end
+   def self.all
+    @@all
+   end 
+   
+   def all_reviews
+    Review.all.select {|all_review| all_review.restaurant == self}
+   end 
+    def list_all_customers
+      Review.all.select {|list_all_customers| list_all_customers.restaurant == self}.uniq
+    end 
+   
 
 end
